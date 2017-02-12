7f5e8c95d830e708b8f04639394c15f96e12e901
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-12 13:03:42 -0800

    reverted back to original configuration, still not seeing changes in heroku

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
 var mongoose = require("mongoose");
-//mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
-mongoose.connect("mongodb://heroku_vjg09pbs:4rtk2e90pb7gp3iopthti0k7lb@ds147789.mlab.com:47789/heroku_vjg09pbs" || "mongodb://localhost/personal-api")
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 module.exports.Goal = require("./goal.js");
