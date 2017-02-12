39e2216d6733692b8c7770ab334a2bf0bc9ae606
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-11 17:16:36 -0800

    Changed Mongodb_uri in index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://heroku_vjg09pbs:4rtk2e90pb7gp3iopthti0k7lb@ds147789.mlab.com:47789/heroku_vjg09pbs");
 
 module.exports.Goal = require("./goal.js");
