bb1eb37e01f6b4e155d0e281d9bda0cf856161ed
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-13 05:50:00 -0800

    create new goal is working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,9 +16,19 @@ $(document).ready(function(){
 		$.ajax({
 			method: "POST",
 			url: "/api/goals",
+			data: $(this).serialize(),
 			success: createGoalSuccess
 		})
 	})
+
+	//Change goal to done
+	$(".isDoneYet").change(function(){
+		$.ajax({
+			method: "PUT",
+			url: "/api/goals/:goal_id",
+			success: changeDoneStatus
+		});
+	});
 
 
 
@@ -28,12 +38,16 @@ $(document).ready(function(){
 
 });
 
+function render(){
+
+}
+
 function showGoalsSuccess(json) {
 	var goals = json;
 	goals.forEach(function(goal){
 		$(".goals")
-		.append(`<div class="card col-sm-6">
-				<div class="card-block">
+		.prepend(`<div class="card col-sm-6">
+				<div class="card-block" data-id="${goal._id}">
 				<h4>What's the goal?</h4>
 				<p>${goal.goal}</p>
 				<h4>Why is it important?</h4>
@@ -43,7 +57,38 @@ function showGoalsSuccess(json) {
 				<h4>What's your reward for getting it done?</h4>
 				<p>${goal.reward}</p>
 				<p>Have you done this yet? <input type="checkbox" class="isDoneYet" value="${goal.completed}"></p>
+				<button class="btn btn-outline-info btn-lg">Edit Goal</button>
 				</div>
 			</div>`)
 	})
+}
+
+function changeDoneStatus(json) {
+	var status = $(this).val();
+	if($(this).attr('checked')) {
+		status = "true";
+	} else {
+		status = "false";
+	}
+	// status = Boolean(status);
+	// goal.completed = status;
+}
+
+function createGoalSuccess(json){
+	var goal = json;
+	$(".goals")
+		.prepend(`<div class="card col-sm-6">
+				<div class="card-block" data-id="${goal._id}">
+				<h4>What's the goal?</h4>
+				<p>${goal.goal}</p>
+				<h4>Why is it important?</h4>
+				<p>${goal.whyImportant}</p>
+				<h4>What's your deadline?</h4>
+				<p>${goal.completeByDate}</p>
+				<h4>What's your reward for getting it done?</h4>
+				<p>${goal.reward}</p>
+				<p>Have you done this yet? <input type="checkbox" class="isDoneYet" value="${goal.completed}"></p>
+				<button class="btn btn-outline-info btn-lg">Edit Goal</button>
+				</div>
+			</div>`)
 }
